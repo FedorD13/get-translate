378f9fa09aeda5af9028a28fa4dacee02d6e8eee
FedorD <fedord.vn@gmail.com>
2020-01-23 14:37:40 +0700

    Initial commit

---
+++ src/main/java/com/translate/getgoogletranslate/GetGoogleTranslateApplication.java
@@ -0,0 +1,13 @@
+package com.translate.getgoogletranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetGoogleTranslateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetGoogleTranslateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/translate/getgoogletranslate/GetGoogleTranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translate.getgoogletranslate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetGoogleTranslateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
