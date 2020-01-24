6d79735be4bba9d4bb02362b421a3eff71eac5ec
FedorD <fedord.vn@gmail.com>
2020-01-24 11:00:39 +0700

    Add ApiInfo

--- src/main/java/com/translate/getgoogletranslate/config/SwaggerConfig.java
+++ src/main/java/com/translate/getgoogletranslate/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.translate.getgoogletranslate.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,15 +17,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket apiDocket() {
-
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.translate.getgoogletranslate.web.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiEndPointInfo());
 
         return docket;
+    }
+
+    private ApiInfo apiEndPointInfo(){
+        return new ApiInfoBuilder().title("Spring Boot Rest API")
+                .description("Google Cloud Translator API")
+                .contact(new Contact("FedorD", "example.com", "example@example.com"))
+                .version("0.0.1-SNAPSHOT")
+                .build();
     }
 }
